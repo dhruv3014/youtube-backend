ca93de1acb94aa4c8cb48b1b712555fc04b75692
Dhruv Sanghani <dtu.18.2024@gmail.com>
2025-06-29 20:48:53 +0530

    connect to Mongo atlas

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// require('dotenv').config({path: '/.env'})
+import dotenv from 'dotenv'
+
+// import mongoose from "mongoose";
+// import { DB_NAME } from "./constants"
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
+
+/*
+import express from "express"
+const app = express()
+
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.PORT.MONGODB_URI}/${DB_NAME}`)
+        app.on("error", (error) => {
+            console.log("ERROR:", error);
+            throw error
+        })
+
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+
+    } catch (error) {
+        console.error("ERROR: ", error)
+        throw err
+    }
+})()
+
+Note : we always use try and catch, async await keyword to handle error coming due to receiving wrong data/URL 
+from database or MONGODB connection error.
+*/
