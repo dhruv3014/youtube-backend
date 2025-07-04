c7879384704e81e79cdd7141e630ae789b1dc2b7
Dhruv Sanghani <dtu.18.2024@gmail.com>
2025-07-04 13:18:40 +0530

    fixed register controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler( async (req, res) => {
     }
     
     // 3. check if user already exists: username, email
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
     if (existedUser) {
@@ -27,7 +27,11 @@ const registerUser = asyncHandler( async (req, res) => {
     
     // 4. check for images, check for avatar
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage?.path;
+    // const coverImageLocalPath = req.files?.coverImage?.path;
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required")
     }
